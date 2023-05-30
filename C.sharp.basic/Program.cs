a6deee6df6899fd7041c5b9fb6f18f2b7ad6087f
WhatIsFun <mohammed110051@gmail.com>
2023-05-30 14:12:41 +0400

    v1.2

--- C.sharp.basic/Program.cs
+++ C.sharp.basic/Program.cs
@@ -2,10 +2,31 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Name: Mohammed");
-        Console.WriteLine("Father Name: Said");
-        Console.WriteLine("DOB: 30 May 1999");
-        Console.WriteLine("Email: mohammed110051@gmail.com");
-        Console.WriteLine("Github Username: WhatIsFun");
+        Console.WriteLine("Enter your first name:");
+        string fName = Console.ReadLine();
+        Console.WriteLine("Enter your family name:");
+        string familyName = Console.ReadLine();
+        Console.WriteLine("Enter your birthday date:");
+        string bD = Console.ReadLine();
+        Console.WriteLine("Enter your Email:");
+        string email = Console.ReadLine();
+        Console.WriteLine("Enter your Github name:");
+        string gName = Console.ReadLine();
+        Console.WriteLine("*               *");
+        Console.WriteLine(" *             * ");
+        Console.WriteLine("  *           *  ");
+        Console.WriteLine("    *       *    ");
+        Console.WriteLine("      *   *      ");
+        Console.WriteLine("        *        ");
+        Console.WriteLine("Hello, programmer");
+        Console.WriteLine("");
+        Console.WriteLine("First Name: " + fName );
+        Console.WriteLine("Family Name: " + familyName);
+        Console.WriteLine("Day of birth: " + bD);
+        Console.WriteLine("*        *  *  *        *");
+        Console.WriteLine("Email: " + email);
+        Console.WriteLine("Github Username:" + gName);
+        Console.ReadLine();
+
     }
 }
