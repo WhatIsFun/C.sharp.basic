94ad670226c948cb7f271ddd7a67628f4d15cb25
WhatIsFun <mohammed110051@gmail.com>
2023-05-30 11:25:48 +0400

    Names

---
+++ C.sharp.basic/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Name: Mohammed");
+        Console.WriteLine("Father Name: Said");
+        Console.WriteLine("DOB: 30 May 1999");
+        Console.WriteLine("Email: mohammed110051@gmail.com");
+        Console.WriteLine("Github Username: WhatIsFun");
+    }
+}
